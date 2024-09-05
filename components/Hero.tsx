e6bf2a900c6a4aa647fa811fcc15f67fe3b6f762
Chien Jui Chen <cxc3787@mavs.uta.edu>
2024-09-05 12:33:38 -0700

    buttons

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -24,16 +24,16 @@ export function TypewriterEffectSmoothDemo() {
       </p>
       <TypewriterEffectSmooth words={words} />
       <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
-        <a href="https://a0ngksn2f5g.typeform.com/to/gdRj57UV#walletaddress=xxxxx">
+        {/* <a href="">
           <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
             Join now
           </button>
         </a>
-        <a href="https://app.gitbook.com/o/BNuVX7CrXwwVRVfoPJYx/s/fd5B7xCsSSRmyiwROZaJ/">
+        <a href="">
         <button className="w-40 h-10 rounded-xl bg-white text-black border border-black  text-sm">
           Document
           </button>
-        </a>
+        </a> */}
       </div>
     </div>
   );
