6ff7dd74392084d7dbfea004a71df0393235514b
Chien Jui Chen <cxc3787@mavs.uta.edu>
2024-11-05 16:06:09 -0800

    Navbar Updated

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,14 +18,15 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-0 inset-x-0 h-16 flex items-center justify-between px-4 md:px-8 z-50", className)}
     >
 <div className="text-2xl font-bold flex items-center">
-        <Image 
-          src="/spongenobg.png"
-          alt="Sponge Logo"
-          width={60}  
-          height={60} 
-          className="mr-2" 
-        />
-        <Link href="https://x.com/spongedotsol">Sponge</Link>
+        <Link href="https://x.com/spongedotsol">
+          <Image 
+            src="/spongeColor.png"
+            alt="Sponge Logo"
+            width={250}  
+            height={250} 
+            className="mr-2" 
+          />
+        </Link>
       </div>
       <Link 
           href="https://app.spongehq.xyz/" 
