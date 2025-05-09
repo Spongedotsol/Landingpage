dc7147d1eb2a742410e7afd4916385e3875c46b7
Chien Jui Chen <cxc3787@mavs.uta.edu>
2025-05-08 22:44:22 -0700

    favicon

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { twMerge } from "tailwind-merge";
 
 export const metadata: Metadata = {
   title: "Sponge",
-  description:"Decentralized Stablecoin on SVM",
+  description:"Decentralized Stablecoins on SVM",
   icons: {
     icon: '/circlesponge.png',
   },
